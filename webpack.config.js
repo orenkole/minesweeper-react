b594bfe6b29e8226ab55081659cc87f6ecb48d46
badger <badger@badger.local>
2022-01-05 16:49:16 +0200

    18. Webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.tsx',
@@ -19,7 +20,23 @@ module.exports = {
 			{
 				test: /\.css&/i,
 				use: ['css-loader']
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+						}
+					}
+				]
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		})
+	]
 }
