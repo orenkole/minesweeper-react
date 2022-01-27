b77d2267c89ee7fa1934b868b68db5a4ae35ef1b
badger <badger@badger.local>
2022-01-27 14:47:21 +0200

    26. Library Emotion for css-in-js

---
+++ src/components/Legend.tsx
@@ -0,0 +1,32 @@
+import React, {FC} from "react";
+import styled from "@emotion/styled";
+
+export const Legend: FC = () => {
+	return (
+		<Parent>
+			<strong>flag: </strong>
+			<FlagComboParent>
+				<Key>ctrl</Key> + <Click>click</Click>
+			</FlagComboParent>
+		</Parent>
+	)
+}
+
+const FlagComboParent = styled.code`
+	background: grey;
+`;
+
+const Parent = styled.legend`
+	font-size: 1em;
+	margin: 0 auto 2vw,
+	line-height: 1.25em,
+`;
+
+const Key = styled.span`
+	color: red;
+
+`;
+
+const Click = styled.span`
+	color: blue;
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import {App} from "./App";
+import { Legend } from "./components/Legend";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Legend />, document.getElementById('root'))
