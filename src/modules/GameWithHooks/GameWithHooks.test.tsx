d59953b94bc166d2e5bbb51ce2972bb679f603bc
badger <badger@badger.local>
2022-02-03 17:43:18 +0200

    63. Test refactoring

--- src/modules/GameWithHooks/GameWithHooks.test.tsx
+++ src/modules/GameWithHooks/GameWithHooks.test.tsx
@@ -2,16 +2,55 @@ import React from "react";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { GameWithHooks } from "./GameWithHooks";
-import { CellState } from "@/helpers/Field";
 
-const {empty: e, hidden: h, bomb: b, flag: f} = CellState;
+const mockOnClick = jest.fn();
+const mockOnChangeLevel = jest.fn();
+const mockOnReset = jest.fn();
 
-jest.mock("@/helpers/Field");
+jest.mock('./useGame', () => ({
+	__esModule: true,
+	useGame: () => ({
+		level: 'beginner',
+		isGameOver: 'beginner',
+		isWin: false,
+		settings: [9, 10],
+		playerField: [
+			[10, 10],
+			[10, 10],
+		],
+		onClick: mockOnClick,
+		onChangeLevel: mockOnChangeLevel,
+		onReset: mockOnReset,
+	})
+}))
+
+beforeEach(() => {
+	jest.clearAllMocks()
+})
 
 describe("GameWithHooks test cases", () => {
-	describe("Render game field by default", () => {})
-	it("Cell click works fine", () => {})
-	it("Reset handler works fine", () => {})
-	it("Change level works fine", () => {})
-	it("Game over works fine", () => {})
+	it("Render game field by default", () => {
+		const {asFragment} = render(<GameWithHooks />)
+		expect(asFragment).toMatchSnapshot();
+	})
+	it("Cell click works fine", () => {
+		render(<GameWithHooks />)
+		userEvent.click(screen.getByTestId('0,0'))
+		expect(mockOnClick).toHaveBeenCalled();
+	})
+	it("Reset handler works fine", () => {
+		render(<GameWithHooks />)
+		userEvent.click(screen.getByRole('button'))
+		expect(mockOnReset).toHaveBeenCalled();
+	})
+	it("Change level works fine", () => {
+		render(<GameWithHooks />)
+		userEvent.selectOptions(screen.getByRole('combobox'), 'intermediate');
+		expect(mockOnChangeLevel).toHaveBeenCalled();
+	})
+	it("Game over works fine", () => {
+		render(<GameWithHooks />)
+		userEvent.click(screen.getByText('🙁'));
+		expect(mockOnReset).toHaveBeenCalled();
+	})
 })
